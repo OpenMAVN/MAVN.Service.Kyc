c22718282444dfe8aee1edb463cb0db1a695ff5f
anna lizunova <a.lizunova@itransition.com>
2020-05-18 16:58:16 +0300

    Create service by tamplate

---
+++ client/MAVN.Service.Kyc.Client/IKycApi.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Refit;
+
+namespace MAVN.Service.Kyc.Client
+{
+    // This is an example of service controller interfaces.
+    // Actual interface methods must be placed here (not in IKycClient interface).
+
+    /// <summary>
+    /// Kyc client API interface.
+    /// </summary>
+    [PublicAPI]
+    public interface IKycApi
+    {
+    }
+}
---
+++ src/MAVN.Service.Kyc/Settings/KycSettings.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using Lykke.SettingsReader.Attributes;
+
+namespace MAVN.Service.Kyc.Settings
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public class KycSettings
+    {
+        public DbSettings Db { get; set; }
+
+        public RabbitMqSettings Rabbit { get; set; }
+    }
+}
